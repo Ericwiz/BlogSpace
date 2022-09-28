f72c10bb7190de15e953a794550dbdf8fe690c5a
Ericwiz <wisdomagu44@gmail.com>
2022-09-28 17:57:22 +0100

    Javascript refactored

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const formBtn = document.getElementById('form');
 const title = document.getElementById('title');
 const body = document.getElementById('body');
 
+// array to hold the post
+let postArray = []
 
-// use fetch to get data
-fetch('https://apis.scrimba.com/jsonplaceholder/posts', { method: "GET", })
-    .then(respone => respone.json())
-    .then(data => {
-        const fivePost = data.slice(0, 10)
-        let html = ''
+function renderPosts() {
+
+    let html = ''
         
-        for (let post of fivePost) {
+        for (let post of postArray) {
             html += `
             <h3>${post.title}</h3>
             <p>${post.body}</p>
@@ -19,6 +18,15 @@ fetch('https://apis.scrimba.com/jsonplaceholder/posts', { method: "GET", })
         }
 
         document.getElementById('display').innerHTML = html;
+}
+
+
+// use fetch to get data
+fetch('https://apis.scrimba.com/jsonplaceholder/posts', { method: "GET", })
+    .then(respone => respone.json())
+    .then(data => {
+        postArray = data.slice(0, 5)
+        renderPosts()
     });
 
 
@@ -26,28 +34,24 @@ fetch('https://apis.scrimba.com/jsonplaceholder/posts', { method: "GET", })
 formBtn.addEventListener('submit', e => {
     e.preventDefault()
 
-    const post = {
+    const data = {
         title: title.value,
         body: body.value
     }
 
-    console.log(post.title)
-    console.log(post.body)
-
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
     method: "POST",
-    body: JSON.stringify(post),
+    body: JSON.stringify(data),
     headers: {
         'Content-Type': 'application/json'
       }
 
 })
     .then(res => res.json())
-        .then(data => {  
-            document.getElementById('display').innerHTML += `
-            <h3>${data.title}</h3>
-            <p>${data.body}</p>
-        `;
+        .then(post => {  
+            
+            postArray.splice(0, 0,post)
+            renderPosts()
         });
 })
 
